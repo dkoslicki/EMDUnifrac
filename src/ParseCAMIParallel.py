db6824606c49eca6fc62bf1118abbed49fd07b8b
dkoslicki <david.koslicki@math.oregonstate.edu>
2017-03-30 22:48:01 -0700

    speed up ParseCAMI example

--- src/ParseCAMIParallel.py
+++ src/ParseCAMIParallel.py
@@ -36,23 +36,31 @@ def make_dist_mat(files_file, output, threshold):
 	fid.close()
 
 	D = np.zeros((len(files), len(files)))
-	#profiles = []
-	#for i in xrange(len(files)):
-	#	profiles.append(PF.Profile(files[i]))
+	profiles = dict()
+	for i in xrange(len(files)):
+		if os.path.exists(files[i]):
+			profile = PF.Profile(files[i])
+			profile.normalize()
+			if threshold > 0:
+				profile.threshold(threshold)
+				profiles[i] = profile
 
+
+	# This is the lazy way to do it, I should really import everything, threshold it all, and then do pw comparisons
+	# That will help with the speed
 	for i in xrange(len(files)):
 		for j in xrange(i+1, len(files)):
 			print("on pair (%d,%d)" % (i, j))
 			in_file_1 = files[i]
 			in_file_2 = files[j]
 			if os.path.exists(in_file_1) and os.path.exists(in_file_2):
-				#profile1 = profiles[i]
-				#profile2 = profiles[j]
-				profile1 = PF.Profile(in_file_1)
-				profile2 = PF.Profile(in_file_2)
-				if threshold > 0:
-					profile1.threshold(threshold)
-					profile2.threshold(threshold)
+				profile1 = profiles[i]
+				profile2 = profiles[j]
+				#profile1 = PF.Profile(in_file_1)
+				#profile2 = PF.Profile(in_file_2)
+				#if threshold > 0:
+				#	profile1.threshold(threshold)
+				#	profile2.threshold(threshold)
 				(Tint, lint, nodes_in_order, nodes_to_index, prob1, prob2) = profile1.make_unifrac_input_and_normalize(profile2)
 				(Z, F, diffab) = EMDU.EMDUnifrac_weighted_flow(Tint, lint, nodes_in_order, prob1, prob2)
 				#(Z, diffab) = EMDU.EMDUnifrac_weighted(Tint, lint, nodes_in_order, prob1, prob2)
